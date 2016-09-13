41d422ca23478fceaf8d6d8e0d127dc4bd9cb4e2
Sal Olivares <salisonhismac@gmail.com>
2016-09-13 15:19:43 -0700

    updated animations

--- js/animations.js
+++ js/animations.js
@@ -1,18 +1,16 @@
 ;(function ($) {
   'use strict';
-  var content  = $('#main').smoothState({
-        // onStart runs as soon as link has been activated
+  var $body    = $('html, body'), // Define jQuery collection
+      content  = $('#main').smoothState({
         onStart : {
-
-          // Set the duration of our animation
           duration: 250,
-
-          // Alterations to the page
           render: function () {
-
-            // Quickly toggles a class and restarts css animations
             content.toggleAnimationClass('is-exiting');
+
+            // Scroll user to the top
+            $body.animate({ 'scrollTop': 0 });
+
           }
         }
-      }).data('smoothState'); // makes public methods available
+      }).data('smoothState');
 })(jQuery);
